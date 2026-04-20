ba01cbaf184cbcb1c5043488c52ee9abd704af94
joaoamiguel <joaopedroamiguel@gmail.com>
2026-04-20 03:52:40 -0300

    animação de corrida do personagem

--- primeiro jogo/Assets/Scripts/PlayerMovement.cs
+++ primeiro jogo/Assets/Scripts/PlayerMovement.cs
@@ -8,28 +8,35 @@ public class PlayerMovement : MonoBehaviour
     AudioSource audio;
     public float speed;
     public static int player_health = 1;
+    private Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         audio = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        animator.SetBool("IsRunning", true);
+        
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
         rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
+        animator.SetFloat("InputX", moveHorizontal);
+        animator.SetFloat("InputY", moveVertical);
+        
         if (GameController.gameOver == true)
         {
             UIController.StopTimer();
             EndGame.Die();
         }
     }
-
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Coletavel")
